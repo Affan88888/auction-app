69a5fb3215d24cdefc71ab813cd2271907b6e1c3
Affan88888 <affankapidzic3@gmail.com>
2025-02-17 18:30:35 +0100

    dodani kodovi za pravljenje tabela u sqltest.py fajlu

--- backend/sqltest.py
+++ backend/sqltest.py
@@ -2,13 +2,14 @@ import mysql.connector
 
 db = mysql.connector.connect(
        host="localhost",
-       user="affan",
-       passwd="Venvscripts333_",
-       database="user_auth_db"
+       user="your_database_user_here",
+       passwd="your_database_password_here",
+       database="your_database_name_here"
     )
 
 mycursor = db.cursor()
 
+# KOD ZA PRAVLJENJE auction_images table-a
 #create_table_query = """
 #         CREATE TABLE auction_images (
 #           id INT AUTO_INCREMENT PRIMARY KEY,
@@ -18,6 +19,18 @@ mycursor = db.cursor()
 #         )
 #       """
 
+# KOD ZA PRAVLJENJE auctions table-a
+#create_table_query = """
+#CREATE TABLE auctions (
+#    id INT AUTO_INCREMENT PRIMARY KEY,
+#    title VARCHAR(255) NOT NULL,
+#    description TEXT NOT NULL,
+#    starting_price DECIMAL(10, 2) NOT NULL,
+#    end_date DATETIME NOT NULL,
+#    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#)
+#"""
+
 #mycursor.execute(create_table_query)
 
 #mycursor.execute("CREATE DATABASE user_auth_db")
@@ -26,7 +39,7 @@ mycursor = db.cursor()
 
 #mycursor.execute("DESCRIBE Person")
 
-#mycursor.execute("INSERT INTO users (username, email, password_hash) VALUES (%s,%s,%s)", ('john_doe', 'john@example.com', 'hashed_password'))
+#mycursor.execute("INSERT INTO users (username, email, password_hash, role) VALUES (%s,%s,%s,%s)", ('username', 'email', 'hashed_pass', 'role'))
 
 #mycursor.execute("DELETE FROM users")
 #db.commit()
