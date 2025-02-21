70163e3c00ebb81b3255f58bab489b7b698d8725
Affan88888 <affankapidzic3@gmail.com>
2025-02-21 18:59:15 +0100

    promijenjen sqltest.py - dodana komanda za bids table

--- backend/sqltest.py
+++ backend/sqltest.py
@@ -31,11 +31,34 @@ mycursor = db.cursor()
 #)
 #"""
 
+# KOD ZA PRAVLJENJE bids table-a
+#create_table_query = """
+#CREATE TABLE bids (
+#    id INT AUTO_INCREMENT PRIMARY KEY,
+#    auction_id INT NOT NULL,
+#    user_id INT NOT NULL,
+#    amount DECIMAL(10, 2) NOT NULL,
+#    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+#    FOREIGN KEY (auction_id) REFERENCES auctions(id),
+#    FOREIGN KEY (user_id) REFERENCES users(id)
+#)
+#"""
+
+# KOD ZA PRAVLJENJE users table-a
+#create_table_query = """
+#CREATE TABLE users (
+#    id INT AUTO_INCREMENT PRIMARY KEY,
+#    username VARCHAR(80) NOT NULL UNIQUE,
+#    email VARCHAR(120) NOT NULL UNIQUE,
+#    password_hash VARCHAR(128) NOT NULL,
+#    role ENUM('user', 'admin') DEFAULT 'user',
+#    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#)
+#"""
+
 #mycursor.execute(create_table_query)
 
 #mycursor.execute("CREATE DATABASE user_auth_db")
-
-#mycursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(80) NOT NULL UNIQUE,email VARCHAR(120) NOT NULL UNIQUE,password_hash VARCHAR(128) NOT NULL,role ENUM('user', 'admin') DEFAULT 'user',created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)")
 
 #mycursor.execute("DESCRIBE Person")
 
