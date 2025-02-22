8d4e451d276a3a2a6f22b9384eea84edf3ae00c0
Affan88888 <affankapidzic3@gmail.com>
2025-02-22 17:37:30 +0100

    updateo sqltest.py

--- backend/sqltest.py
+++ backend/sqltest.py
@@ -9,15 +9,17 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-# KOD ZA PRAVLJENJE auction_images table-a
+# KOD ZA PRAVLJENJE users table-a
 #create_table_query = """
-#         CREATE TABLE auction_images (
-#           id INT AUTO_INCREMENT PRIMARY KEY,
-#            auction_id INT NOT NULL,
-#            image_url VARCHAR(255) NOT NULL,
-#            FOREIGN KEY (auction_id) REFERENCES auctions(id) ON DELETE CASCADE
-#         )
-#       """
+#CREATE TABLE users (
+#    id INT AUTO_INCREMENT PRIMARY KEY,
+#    username VARCHAR(80) NOT NULL UNIQUE,
+#    email VARCHAR(120) NOT NULL UNIQUE,
+#    password_hash VARCHAR(128) NOT NULL,
+#    role ENUM('user', 'admin') DEFAULT 'user',
+#    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#)
+#"""
 
 # KOD ZA PRAVLJENJE auctions table-a
 #create_table_query = """
@@ -31,6 +33,16 @@ mycursor = db.cursor()
 #)
 #"""
 
+# KOD ZA PRAVLJENJE auction_images table-a
+#create_table_query = """
+#         CREATE TABLE auction_images (
+#           id INT AUTO_INCREMENT PRIMARY KEY,
+#            auction_id INT NOT NULL,
+#            image_url VARCHAR(255) NOT NULL,
+#            FOREIGN KEY (auction_id) REFERENCES auctions(id) ON DELETE CASCADE
+#         )
+#       """
+
 # KOD ZA PRAVLJENJE bids table-a
 #create_table_query = """
 #CREATE TABLE bids (
@@ -39,20 +51,8 @@ mycursor = db.cursor()
 #    user_id INT NOT NULL,
 #    amount DECIMAL(10, 2) NOT NULL,
 #    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#    FOREIGN KEY (auction_id) REFERENCES auctions(id),
-#    FOREIGN KEY (user_id) REFERENCES users(id)
-#)
-#"""
-
-# KOD ZA PRAVLJENJE users table-a
-#create_table_query = """
-#CREATE TABLE users (
-#    id INT AUTO_INCREMENT PRIMARY KEY,
-#    username VARCHAR(80) NOT NULL UNIQUE,
-#    email VARCHAR(120) NOT NULL UNIQUE,
-#    password_hash VARCHAR(128) NOT NULL,
-#    role ENUM('user', 'admin') DEFAULT 'user',
-#    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#    FOREIGN KEY (auction_id) REFERENCES auctions(id) ON DELETE CASCADE,
+#    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 #)
 #"""
 
